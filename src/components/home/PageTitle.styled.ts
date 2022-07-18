28ed020840c6d5cdc6e34a22fd88197c4e5db71c
Jung Jin Sol <smokerjs.dev@gmail.com>
2022-07-18 22:40:59 +0900

    타이틀영역 레이아웃

--- src/components/home/PageTitle.styled.ts
+++ src/components/home/PageTitle.styled.ts
@@ -1,5 +1,15 @@
 import styled from 'styled-components';
 
-const TitleSection = styled.section`
-  position: absolute;
+export const TitleSection = styled.section`
+  position: fixed; 
+  left: 50%; 
+  top: 50%; 
+  transform: translate(-50%, -50%);
 `;
+
+export const TitleText = styled.h1`
+  font-size: 4vw;
+  white-space: pre;
+  font-weight: 400;
+`;
+
--- src/components/home/PageTitle.tsx
+++ src/components/home/PageTitle.tsx
@@ -1,8 +1,16 @@
 import React, { FC } from 'react';
 import * as $ from './PageTitle.styled';
 
+const TITLE_TEXT = 'I AM A\nFRONT-END\nDEVELOPER';
+
 const PageTitle: FC = () => {
-  return <>I am a front-end developer</>;
+  return (
+    <$.TitleSection>
+      <$.TitleText>
+        {TITLE_TEXT}
+      </$.TitleText>
+    </$.TitleSection>
+  );
 };
 
 export default PageTitle;
