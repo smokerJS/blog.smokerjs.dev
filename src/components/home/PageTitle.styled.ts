8d9418ffc70f36d1f056e382dc18f48c3e7bf7b9
Jung Jin Sol <smokerjs.dev@gmail.com>
2022-07-19 12:32:01 +0900

    폰트사이즈 미디어쿼리 추가

--- src/components/home/PageTitle.styled.ts
+++ src/components/home/PageTitle.styled.ts
@@ -1,15 +1,17 @@
 import styled from 'styled-components';
 
 export const TitleSection = styled.section`
-  position: fixed; 
-  left: 50%; 
-  top: 50%; 
+  position: fixed;
+  left: 50%;
+  top: 50%;
   transform: translate(-50%, -50%);
 `;
 
 export const TitleText = styled.h1`
-  font-size: 4vw;
+  @media (min-width: 1024px) {
+    font-size: 100px;
+  }
+  font-size: 8vw;
   white-space: pre;
   font-weight: 400;
 `;
-
