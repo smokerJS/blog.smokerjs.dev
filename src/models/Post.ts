91b4911f0dc4e640a1a54c6e55081007d70afa3c
Jung Jin Sol <smokerjs.dev@gmail.com>
2022-07-31 00:31:53 +0900

    타입 => 모델 수정 및 기본타입 상속

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -3,17 +3,13 @@ export interface PostSummary {
   title: string;
   date: string;
 }
+export interface Post extends PostSummary {
+  tags: string;
+  categoryName: string;
+  contentHtml: string;
+}
 
 export interface Category {
   name: string;
   postSummaries: PostSummary[];
-}
-
-export interface Post {
-  id: string;
-  title: string;
-  date: string;
-  tags: string;
-  categoryName: string;
-  contentHtml: string;
 }
